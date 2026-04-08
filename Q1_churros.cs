5d38ea1a13f58798e63ae9005db91005381d1b6f
Adith Ravi <adithravi23.ie@gmail.com>
2026-04-08 02:59:49 +0100

    Create Churros class with properties and constructor

---
+++ Q1_churros.cs
@@ -0,0 +1,11 @@
+public class Churros
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+
+    public Churros(string name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+}
